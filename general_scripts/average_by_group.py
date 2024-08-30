b521068703efcc0a40563d7753cf00a21e00f8ff
Divya-Aggarwal008 <divya_a@bt.iitr.ac.in>
2024-08-30 10:05:10 +0530

    Create average_by_group.py

---
+++ general_scripts/average_by_group.py
@@ -0,0 +1,5 @@
+def average_by_group(in_file):
+  import pandas as pd
+  df = pd.read_csv(in_file, sep='\t', header = none, names=['value','category'])
+  avg_by_group = df.groupby('category').mean()
+  return avg_by_group
